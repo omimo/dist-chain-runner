8900e0e3ec68809bf58a17375d72efcf3e1f1554
Omid Alemi <omid.alemi@gmail.com>
2019-11-26 17:41:28 -0800

    added the updated webcam sender

--- send_webcam.py
+++ send_webcam.py
@@ -10,24 +10,23 @@ import sys
 import numpy as np
 import cv2
 
-TARGET_IP = '127.0.0.1'
+TARGET_IP = '142.58.206.30'
 TARGET_PORT = 5000
 URL_SEND_PHOTO = 'http://%s:%d/newphoto'%(TARGET_IP, TARGET_PORT)
 
 
 
 CAM_SIZE = 1280
-cam = cv2.VideoCapture(0)
-cam.set(3, CAM_SIZE)
-cam.set(4, CAM_SIZE/1.6)
 
 
 
-def send_photo(index, name, img_file):
+
+def send_photo(index, name, code_id, img_file):
 
     post_data = {
         'index': index,
-        'name': name
+        'name': name,
+        'code_id': code_id
     }
 
     with open(img_file, 'rb') as _img_file:
@@ -42,16 +41,23 @@ counter = 1
 while True:
     print('Enter the name:')
     name = input() 
+    
+    print('Enter the code:')
+    code_id = input() 
 
+    cam = cv2.VideoCapture(0)
+    cam.set(3, CAM_SIZE)
+    cam.set(4, CAM_SIZE/1.6)
     retval, img = cam.read()
+    cam.release()
 
     print('retval: ')
     print(retval)
     print(img.shape)
 
-    cv2.imwrite('/tmp/cam.png', img)
+    cv2.imwrite('/tmp/cam.jpg', img)
 
-    send_photo(counter, name, '/tmp/cam.png')
+    send_photo(counter, name, code_id, '/tmp/cam.jpg')
 
     print('Sent the photo # %d'%counter)
 
